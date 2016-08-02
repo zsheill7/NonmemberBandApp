3051261aa31a11b453deacd4bca094064336e9b1
zsheill7 <zsheill7@gmail.com>
2016-08-02 12:42:00 -0400

    Mass commit 2

--- Nonmember Band App/TwitterViewController.swift
+++ Nonmember Band App/TwitterViewController.swift
@@ -14,19 +14,30 @@ class TwitterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        // Add a button to the center of the view to show the timeline
+        let button = UIButton(type: .System)
+        button.setTitle("Show Timeline", forState: .Normal)
+        button.sizeToFit()
+        button.center = view.center
+        button.addTarget(self, action: #selector(showTimeline), forControlEvents: [.TouchUpInside])
+        view.addSubview(button)
+
     }
+    
+    
     
     func showTimeline() {
         // Create an API client and data source to fetch Tweets for the timeline
         let client = TWTRAPIClient()
         //TODO: Replace with your collection id or a different data source
         let dataSource = TWTRCollectionTimelineDataSource(collectionID: "539487832448843776", APIClient: client)
-        // Create the timeline view controller
+
         let timelineViewControlller = TWTRTimelineViewController(dataSource: dataSource)
-        // Create done button to dismiss the view controller
+
         let button = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(dismissTimeline))
         timelineViewControlller.navigationItem.leftBarButtonItem = button
-        // Create a navigation controller to hold the
+
         let navigationController = UINavigationController(rootViewController: timelineViewControlller)
         showDetailViewController(navigationController, sender: self)
     }
